c17a4e0a6898a0e88d925fe90e8533bd5cb38816
Joshua Kapilian <kapgamesandapps2@gmail.com>
2024-01-14 22:58:58 -0800

    Add error handling and change detection

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import urllib.request
 import json
-import csv
+import time
 
 def main():
 	try:
@@ -8,24 +8,36 @@ def main():
 		arr = json.loads(file.read())
 		file.close()
 	except:
-		arr = []
+		arr = {}
 	with urllib.request.urlopen("https://prod-tickets.1iota.com/api/homepage") as response:
 		resp_json = json.loads(response.read())
+		cur_time = time.time()
 		for section in resp_json["pageSections"]:
-			for show in section["items"]:
-				title = show["altText"]
-				path_arr = show["path"].split("/")
-				celeb = path_arr[0] == "fanbase"
-				id = path_arr[2] if celeb else path_arr[1]
-				api_route = "celeb" if celeb else "project"
-				resp_key = "eventList" if celeb else "events"
-				with urllib.request.urlopen(f"https://prod-tickets.1iota.com/api/{api_route}/{id}") as show_response:
-					show_json = json.loads(show_response.read())
-					for event in show_json[resp_key]:
-						if(event["isSoldOut"] == False):
-							if event["eventId"] not in arr:
-								arr.append(event["eventId"])
-								print(f'{title} available on {event["localStartDay"]} at {event["when"]} in {event["where"]}')
+			try:
+				for show in section["items"]:
+					title = show["altText"]
+					path_arr = show["path"].split("/")
+					celeb = path_arr[0] == "fanbase"
+					id = path_arr[2] if celeb else path_arr[1]
+					api_route = "celeb" if celeb else "project"
+					resp_key = "eventList" if celeb else "events"
+					with urllib.request.urlopen(f"https://prod-tickets.1iota.com/api/{api_route}/{id}") as show_response:
+						show_json = json.loads(show_response.read())
+						for event in show_json[resp_key]:
+							eventId = str(event["eventId"])
+							if eventId not in arr.keys():
+								arr[eventId] = {
+									cur_time: event
+								}
+								print(f'New Show: {title} - {event["localStartDay"]} at {event["when"]} in {event["where"]}')
+							elif event != arr[eventId][max(arr[eventId].keys())]:
+								arr[eventId] = {
+									cur_time: event
+								}
+								print(f'Updated: {title} - {event["localStartDay"]} at {event["when"]} in {event["where"]}')
+			except:
+				print(f'Issue with {api_route} {id}')
+				
 		writeFile = open("shows.json", "w")
 		writeFile.write(json.dumps(arr))
 		writeFile.close()
